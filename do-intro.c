4db0406aa545596076dd35f752191e51988b7474
molly memott <memmottjoy@gmail.com>
2025-05-07 14:36:45 +0000

    bgfdgfd

---
+++ do-intro.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Print "Hello World ¡ to the screen 
+int main() {
+
+// Print numbers from 1 to 5
+
+
+  int i = 1;
+
+  while (i <= 5) {
+    printf("%d\n", i);
+    ++i;
+  }
+
+  return 0;
+}
+
+
+
+
